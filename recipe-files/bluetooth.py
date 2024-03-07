77c7c73da987aa89aa61c0c7c821efa229274a74
Ahmed Marzouk <ahmed.marzouk@arcticspas.com>
2024-03-07 12:38:00 -0700

    Add a helper functions file

--- recipe-files/bluetooth.py
+++ recipe-files/bluetooth.py
@@ -1,7 +1,9 @@
-# test_script.py
+from helpers import greet
 
 def main():
     print("Hello, Yocto! This is a bluetooth script, got from GITHUB.")
+    message = greet("Ahmed Mohamed")
+    print(message);
 
 
 if __name__ == "__main__":
---
+++ recipe-files/helpers.py
@@ -0,0 +1,4 @@
+# Helpers functions:
+
+def greet(name):
+    return f"Hello, {name}!"
