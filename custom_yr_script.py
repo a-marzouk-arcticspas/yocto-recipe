f424f132cbd6e21586bd1e365c3ae5048135bc51
Ahmed Marzouk <ahmed.marzouk@arcticspas.com>
2024-03-07 10:59:48 -0700

    simple test py script

---
+++ custom_yr_script.py
@@ -0,0 +1,14 @@
+# test_script.py
+
+def main():
+    print("Hello, Yocto! This is a test script.")
+
+    # Perform a basic calculation
+    result = add_numbers(3, 5)
+    print("Result of adding 3 and 5:", result)
+
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    main()
